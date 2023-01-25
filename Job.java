d02b9f53e250137bf8ecab0d4d6d383bbf7fc27a
IvaNakova <georgieva_iva@abv.bg>
2023-01-25 18:18:29 +0200

    initial state

---
+++ Job.java
@@ -0,0 +1,43 @@
+import java.util.Objects;
+
+public class Job<E> {
+    private Node<E> start;
+    private Node<E> end;
+
+    /**
+     * @param start The start location of a job
+     * @param end The end location of a job
+     */
+    public Job(Node<E> start, Node<E> end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public Node<E> getStart() {
+        return start;
+    }
+
+    public Node<E> getEnd() {
+        return end;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this.getClass().equals(o.getClass())) {
+            @SuppressWarnings("unchecked")
+                    Job<E> j = (Job<E>) o;
+            return this.start.equals(j.start) && this.end.equals(j.end);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(start, end);
+    }
+
+    @Override
+    public String toString() {
+        return start.getName() + "to" + end.getName();
+    }
+}
