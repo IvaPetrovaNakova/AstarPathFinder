51d4faace306583f4e4c3a87933fe90d4d28967c
IvaNakova <georgieva_iva@abv.bg>
2023-01-27 08:50:46 +0200

    added first unfinished project

---
+++ Edge.java
@@ -0,0 +1,77 @@
+/**
+ * The Edge class holds an Edge object, which will store the weight of the edge and the two vertices that
+ * connect it.
+ *
+ */
+
+public class Edge implements Comparable<Edge> {
+    private Vertex v1;
+    private Vertex v2;
+    public double w;
+
+    public Edge(Vertex vert1, Vertex vert2, double weight){
+        v1 = vert1;
+        v2 = vert2;
+        w = weight;
+    }
+
+    /**
+     * Returns the weight of the edge
+     */
+    public double getWeight(){
+        return w;
+    }
+
+    /**
+     * Returns whatever vertex isn't given that shares the same edge.
+     *
+     * @param v     the initial vertex
+     * @return      the other vertex
+     */
+    public Vertex getOther(Vertex v){
+        if(v.city.equals(v1.city)){
+            return v2;
+        }
+        else if (v.city.equals(v2.city)){
+            return v1;
+        }
+        else{
+            return null;
+        }
+    }
+
+    /**
+     * Returns one of the vertices for the edge.
+     */
+    public Vertex getEither(){
+        return v1;
+    }
+
+    /**
+     * The compareTo method compares the weight of the current edge to the weight of another edge.
+     * If the current edge is bigger, it returns 1.
+     * If the current edge is smaller, it returns -1.
+     * If the edges are the same weight, it returns 0.
+     */
+    @Override
+    public int compareTo(Edge other){
+        double temp = w-other.w;
+        if(temp > 0){
+            temp = 1;
+        }
+        else if (temp < 0){
+            temp = -1;
+        }
+        return (int)temp;
+    }
+
+    /**
+     * The toString() method prints general information about the edge
+     *
+     * @return  a string containing the two vertices and the weight
+     */
+    public String toString(){
+        String s = "v1 = " + v1.city + ", v2 = " + v2.city + ", weight = " + w;
+        return s;
+    }
+}
---
+++ Vertex.java
@@ -0,0 +1,137 @@
+import java.util.ArrayList;
+/**
+ * The Vertex class holds a vertex object, which will hold the latitude and longitude of the vertex
+ * along with the name of the city itself.
+ *
+ * @param lat    the latitude
+ * @param llong  the longitude
+ * @param name   the city name
+ *
+ */
+public class Vertex implements Comparable<Vertex> {
+    private double latitude;
+    private double longitude;
+
+    //stores an arraylist of all the edges connected to this vertex
+    private ArrayList<Edge> edges;
+
+    //holds the score of the vertex when come across in a search
+    private double score;
+    private double gx; //holds the distance travelled to this vertex when come across in a search
+    private String city; //city name
+    private Vertex prev; //the vertex that led to this vertex in a search
+    private final int R = 6371; //radius of earth
+    public Vertex (String name, double latitude, double longitude){
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.edges = new ArrayList<Edge>();
+        this.score = 0;
+        this.gx = 0;
+        this.city = name;
+        this.prev = null;
+    }
+
+    /**
+     * Returns a list of the edges connected to this vertex.
+     *
+     * @return  edge ArrayList
+     */
+    public ArrayList<Edge> getEdges(){
+        return edges;
+    }
+
+    /**
+     * @return  the city name
+     */
+    public String toString(){
+        return this.city;
+    }
+
+    /**
+     * The Heur method takes in a vertex and calculates the distance between it and the current vertex using
+     * the formula for distance between two points on a sphere.
+     *
+     * @param v     the other vertex
+     * @return      the crow-flies distance between current vertex and the other vertex
+     */
+    public double heuristicCalculationGeo(Vertex v){
+        //convert all the values from degrees to radians
+        double lata = this.latitude * (Math.PI / 180);
+        double latb = v.latitude*(Math.PI/180);
+        double lona = this.longitude*(Math.PI/180);
+        double lonb = v.longitude*(Math.PI/180);
+        //uses distance formula to calculate the distance
+        double dist = Math.acos(Math.sin(lata) * Math.sin(latb) +
+                Math.cos(lata) * Math.cos(latb) * Math.cos(lona-lonb)) * R;
+        return dist;
+    }
+
+    /**
+     * The recalcScore method calculates the score that the current vertex has in a search by adding its
+     * distance to the heuristic.
+     *
+     * @param goal  the end goal for the search
+     */
+    public void recalcScore(Vertex goal){
+        score = setDist() + heuristicCalculationGeo(goal);
+    }
+
+    /**
+     * setPrev sets the current vertex's prev value
+     * @param v     the vertex's previous value
+     */
+    public void setPrev(Vertex v){
+        prev = v;
+    }
+
+    /**
+     * The setDist method calculates the distance travelled in a search to reach the current vertex.
+     * @return distance travelled
+     */
+    public double setDist(){
+        if (prev == null) {
+            gx = 0;
+        }
+        else{
+            gx = prev.gx + sharedEdge(prev).w;
+        }
+        return gx;
+    }
+
+    /**
+     * the compareTo method compares two vertices based on their current scores, with the smaller score receiving
+     * a value of 1.
+     *
+     * @param v     the other vertex
+     * @return      the position of the current vertex in comparison to the other vertex
+     */
+    @Override
+    public int compareTo(Vertex v) {
+        if (this.score > v.score) {
+            return -1;
+        }
+        else if (this.score < v.score) {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
+
+    /**
+     * The sharedEdge method finds the edge shared between two vertices.
+     * @param v     the other vertex
+     * @return      the shared edge
+     */
+    public Edge sharedEdge(Vertex v){
+        Edge edge = null;
+        //loops through each edge connected to the current vertex, checking to see if the edge
+        //connects the current vertex with the inputted vertex
+        for(int i = 0; i < edges.size(); i++){
+            if(edges.get(i).getOther(this).city.equals(v.city)){
+                edge = edges.get(i);
+            }
+        }
+        return edge;
+    }
+}
